0e2b85e1d16d66700d3ce7b22c4eaa97261e0512
Muki Kiboigo <muki@kiboigo.com>
2024-08-03 00:15:51 -0700

    add Zig build support

---
+++ build.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const prefix = "eigen3/Eigen";
+
+    b.installDirectory(.{
+        .source_dir = b.path("Eigen/"),
+        .install_dir = .{ .custom = prefix },
+        .install_subdir = "",
+    });
+}
