638cc3ac8517709a8663fd3cade9a2b114ccb6e4
Muki Kiboigo <muki@kiboigo.com>
2024-08-03 01:14:19 -0700

    install the headers for Zig

--- build.zig
+++ build.zig
@@ -1,11 +1,10 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
-    const prefix = "eigen3/Eigen";
-
+    const prefix = "eigen3/Eigen/";
     b.installDirectory(.{
         .source_dir = b.path("Eigen/"),
-        .install_dir = .{ .custom = prefix },
-        .install_subdir = "",
+        .install_dir = .header,
+        .install_subdir = prefix,
     });
 }
